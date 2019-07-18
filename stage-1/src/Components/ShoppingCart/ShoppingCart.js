c7941fa4e95fcd097055aef6a0785b592be4a633
User <user@Users-MacBook-Pro.local>
2019-07-18 17:39:09 -0500

    finished debugging

--- stage-1/src/Components/ShoppingCart/ShoppingCart.js
+++ stage-1/src/Components/ShoppingCart/ShoppingCart.js
@@ -3,7 +3,7 @@ import "./ShoppingCart.css";
 
 class ShoppingCart extends Component {
   render() {
-    let shoppingCartDisplay = this.props.cart.map((element, index) => {
+    let shoppingCartDisplay = this.props.shoppingCart.map((element, index) => {
       return (
         <div className="shopping-cart-product-container" key={index}>
           <img src={element.image} alt="" />
